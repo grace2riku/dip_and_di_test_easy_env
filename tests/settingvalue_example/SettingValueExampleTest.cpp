984057fcb88bd09f94273f5255851521a54f5d07
Koji Abe <gracehime@gmail.com>
2023-09-28 01:12:40 +0900

    環境構築確認用に使用したテストを削除

--- tests/settingvalue_example/SettingValueExampleTest.cpp
+++ tests/settingvalue_example/SettingValueExampleTest.cpp
@@ -1,44 +1,32 @@
-
 extern "C"
 {
 }
 
 #include "CppUTest/TestHarness.h"
+
 #include <iostream>
 using namespace std;
-#include "Boot.h"
-#include "Factories.h"
 #include "SettingValueValidation.h"
 #include "SettingValueMock.h"
 #include "ISettingValue.h"
 
 TEST_GROUP(SettingValueExampleTest)
 {
-    Boot* boot;
     SettingValueValidation* settingValueValidation;
-    ISettingValue* settingValue;
+
     void setup()
     {
-      boot = new Boot(Factories::CreateSettingValue());
       settingValueValidation = new SettingValueValidation(new SettingValueMock());
     }
 
     void teardown()
     {
-      delete boot;
       delete settingValueValidation;
     }
 };
 
 TEST(SettingValueExampleTest, SettingValueValid)
 {
-//  FAIL("SettingValueValid fail!!!");
   CHECK(settingValueValidation->validate());
-}
-
-TEST(SettingValueExampleTest, FirstStep)
-{
-//  FAIL("Start here!!!");
-    LONGS_EQUAL(123, boot->readSettingValue());
 }
 
